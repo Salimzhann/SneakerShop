b06a53fcfaba210c4f3a8f47bf1d7db15f2b8c85
Salimzhann <manassalimzhan@icloud.com>
2024-05-29 19:20:07 +0500

    Initial Commit

---
+++ SneakerShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SneakerShop
+//
+//  Created by Manas Salimzhan on 29.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
