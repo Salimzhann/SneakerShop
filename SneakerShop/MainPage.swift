5bb4beff87ce6bbbd5ee2d2ec2f4d186d5f36b8e
Salimzhann <manassalimzhan@icloud.com>
2024-05-31 16:37:05 +0500

    Sale CollectionView is added

--- SneakerShop/MainPage.swift
+++ SneakerShop/MainPage.swift
@@ -8,22 +8,68 @@
 import UIKit
 
 class MainPage: UIViewController {
+    
+    let saleCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.isPagingEnabled = true
+        cv.showsHorizontalScrollIndicator = false
+        cv.register(SaleCell.self, forCellWithReuseIdentifier: SaleCell.identifier)
+        return cv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupSaleCollectionView()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        let menuButton = UIBarButtonItem(image: UIImage(systemName: "line.horizontal.3"),style: .plain, target: self, action: #selector(handleMenu))
+        navigationItem.leftBarButtonItem = menuButton
+        menuButton.tintColor = .black
+        
+        [saleCollectionView].forEach({view.addSubview($0)})
+        [saleCollectionView].forEach({$0.translatesAutoresizingMaskIntoConstraints = false})
+        
+        NSLayoutConstraint.activate([
+            saleCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            saleCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            saleCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            saleCollectionView.heightAnchor.constraint(equalToConstant: 150)
+        ])
+        
+        
     }
-    */
+    
+    @objc func handleMenu() {
+        
+    }
+    
+    func setupSaleCollectionView() {
+        saleCollectionView.dataSource = self
+        saleCollectionView.delegate = self
+    }
 
+}
+
+
+extension MainPage: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = saleCollectionView.dequeueReusableCell(withReuseIdentifier: SaleCell.identifier, for: indexPath) as! SaleCell
+        cell.configure(with: "saleImage_1")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 50, height: 150)
+    }
+    
+    
 }
---
+++ SneakerShop/SaleCell.swift
@@ -0,0 +1,102 @@
+//
+//  SaleCell.swift
+//  SneakerShop
+//
+//  Created by Manas Salimzhan on 31.05.2024.
+//
+
+import UIKit
+
+class SaleCell: UICollectionViewCell {
+    static let identifier: String = "saleCell"
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray5
+        view.layer.cornerRadius = 18
+        view.layer.masksToBounds = false
+        return view
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Shop now", for: .normal)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 16
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(imageView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(subtitleLabel)
+        containerView.addSubview(button)
+        self.clipsToBounds = false
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            imageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            imageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 20),
+            imageView.widthAnchor.constraint(equalToConstant: 250),
+            imageView.heightAnchor.constraint(equalToConstant: 250),
+            
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 25),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
+            
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            subtitleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 25),
+            subtitleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
+            
+            button.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 10),
+            button.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 25),
+            button.widthAnchor.constraint(equalToConstant: 96),
+            button.heightAnchor.constraint(equalToConstant: 29)
+        ])
+    }
+
+    func configure(with title: String) {
+        imageView.image = UIImage(named: "saleImage_1") // Replace with actual image
+        titleLabel.text = "20% Discount"
+        subtitleLabel.text = "on your first purchase"
+    }
+}
+
